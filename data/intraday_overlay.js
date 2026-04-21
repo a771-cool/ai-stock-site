41b47bac01307749eecb7f58772ba4d3a8aea14f
a771-cool <a771986@gmail.com.tw>
2026-04-21 19:48:28 +0800

    update hosted site 2026-04-21 19:48

--- data/intraday_overlay.js
+++ data/intraday_overlay.js
@@ -1,8 +1,271 @@
 ﻿window.STOCK_AI_INTRADAY = {
-    "generatedAt":  "2026-04-02 09:11:31 +0800",
-    "count":  0,
+    "generatedAt":  "2026-04-21 13:20:17 +0800",
+    "count":  33,
     "byCode":  {
-
+                   "1210":  {
+                                "latestPrice":  54.0,
+                                "targetPrice":  59.8,
+                                "supportPrice":  54.1,
+                                "potentialPct":  10.7407,
+                                "rr":  null,
+                                "distanceToSupportPct":  -0.1852
+                            },
+                   "2891":  {
+                                "latestPrice":  53.1,
+                                "targetPrice":  56.9,
+                                "supportPrice":  51.3,
+                                "potentialPct":  7.1563,
+                                "rr":  2.1111,
+                                "distanceToSupportPct":  3.3898
+                            },
+                   "5291":  {
+                                "latestPrice":  67.8,
+                                "targetPrice":  82.2,
+                                "supportPrice":  66.8,
+                                "potentialPct":  21.2389,
+                                "rr":  14.4,
+                                "distanceToSupportPct":  1.4749
+                            },
+                   "1227":  {
+                                "latestPrice":  30.0,
+                                "targetPrice":  31.25,
+                                "supportPrice":  29.5,
+                                "potentialPct":  4.1667,
+                                "rr":  2.5,
+                                "distanceToSupportPct":  1.6667
+                            },
+                   "2385":  {
+                                "latestPrice":  124.5,
+                                "targetPrice":  127.5,
+                                "supportPrice":  119.0,
+                                "potentialPct":  2.4096,
+                                "rr":  0.5455,
+                                "distanceToSupportPct":  4.4177
+                            },
+                   "1907":  {
+                                "latestPrice":  25.45,
+                                "targetPrice":  26.45,
+                                "supportPrice":  23.75,
+                                "potentialPct":  3.9293,
+                                "rr":  0.5882,
+                                "distanceToSupportPct":  6.6798
+                            },
+                   "2313":  {
+                                "latestPrice":  253.5,
+                                "targetPrice":  294.0,
+                                "supportPrice":  222.5357,
+                                "potentialPct":  15.9763,
+                                "rr":  1.308,
+                                "distanceToSupportPct":  12.2147
+                            },
+                   "5244":  {
+                                "latestPrice":  47.05,
+                                "targetPrice":  56.1847,
+                                "supportPrice":  46.975,
+                                "potentialPct":  19.4149,
+                                "rr":  121.796,
+                                "distanceToSupportPct":  0.1594
+                            },
+                   "2338":  {
+                                "latestPrice":  50.1,
+                                "targetPrice":  58.0,
+                                "supportPrice":  44.45,
+                                "potentialPct":  15.7685,
+                                "rr":  1.3982,
+                                "distanceToSupportPct":  11.2774
+                            },
+                   "2014":  {
+                                "latestPrice":  18.3,
+                                "targetPrice":  19.3,
+                                "supportPrice":  17.95,
+                                "potentialPct":  5.4645,
+                                "rr":  2.8571,
+                                "distanceToSupportPct":  1.9126
+                            },
+                   "2890":  {
+                                "latestPrice":  31.85,
+                                "targetPrice":  33.65,
+                                "supportPrice":  30.5,
+                                "potentialPct":  5.6515,
+                                "rr":  1.3333,
+                                "distanceToSupportPct":  4.2386
+                            },
+                   "1723":  {
+                                "latestPrice":  82.9,
+                                "targetPrice":  83.0,
+                                "supportPrice":  76.6,
+                                "potentialPct":  0.1206,
+                                "rr":  0.0159,
+                                "distanceToSupportPct":  7.5995
+                            },
+                   "2002":  {
+                                "latestPrice":  19.75,
+                                "targetPrice":  20.55,
+                                "supportPrice":  18.85,
+                                "potentialPct":  4.0506,
+                                "rr":  0.8889,
+                                "distanceToSupportPct":  4.557
+                            },
+                   "00899":  {
+                                 "latestPrice":  24.52,
+                                 "targetPrice":  24.82,
+                                 "supportPrice":  23.2,
+                                 "potentialPct":  1.2235,
+                                 "rr":  0.2273,
+                                 "distanceToSupportPct":  5.3834
+                             },
+                   "1904":  {
+                                "latestPrice":  18.75,
+                                "targetPrice":  19.2,
+                                "supportPrice":  17.2,
+                                "potentialPct":  2.4,
+                                "rr":  0.2903,
+                                "distanceToSupportPct":  8.2667
+                            },
+                   "2460":  {
+                                "latestPrice":  37.95,
+                                "targetPrice":  37.3,
+                                "supportPrice":  32.2,
+                                "potentialPct":  -1.7128,
+                                "rr":  -0.113,
+                                "distanceToSupportPct":  15.1515
+                            },
+                   "1597":  {
+                                "latestPrice":  114.0,
+                                "targetPrice":  119.5,
+                                "supportPrice":  109.0,
+                                "potentialPct":  4.8246,
+                                "rr":  1.1,
+                                "distanceToSupportPct":  4.386
+                            },
+                   "8261":  {
+                                "latestPrice":  127.5,
+                                "targetPrice":  132.0353,
+                                "supportPrice":  117.5679,
+                                "potentialPct":  3.5571,
+                                "rr":  0.4566,
+                                "distanceToSupportPct":  7.7899
+                            },
+                   "8096":  {
+                                "latestPrice":  93.6,
+                                "targetPrice":  98.7,
+                                "supportPrice":  85.2,
+                                "potentialPct":  5.4487,
+                                "rr":  0.6071,
+                                "distanceToSupportPct":  8.9744
+                            },
+                   "2464":  {
+                                "latestPrice":  85.7,
+                                "targetPrice":  95.132,
+                                "supportPrice":  81.0179,
+                                "potentialPct":  11.0058,
+                                "rr":  2.0145,
+                                "distanceToSupportPct":  5.4634
+                            },
+                   "8103":  {
+                                "latestPrice":  95.7,
+                                "targetPrice":  106.0,
+                                "supportPrice":  92.3,
+                                "potentialPct":  10.7628,
+                                "rr":  3.0294,
+                                "distanceToSupportPct":  3.5528
+                            },
+                   "8070":  {
+                                "latestPrice":  47.85,
+                                "targetPrice":  50.1,
+                                "supportPrice":  46.0,
+                                "potentialPct":  4.7022,
+                                "rr":  1.2162,
+                                "distanceToSupportPct":  3.8662
+                            },
+                   "6166":  {
+                                "latestPrice":  79.4,
+                                "targetPrice":  84.8079,
+                                "supportPrice":  73.0893,
+                                "potentialPct":  6.811,
+                                "rr":  0.8569,
+                                "distanceToSupportPct":  7.948
+                            },
+                   "3406":  {
+                                "latestPrice":  523.0,
+                                "targetPrice":  593.9415,
+                                "supportPrice":  511.1607,
+                                "potentialPct":  13.5643,
+                                "rr":  5.992,
+                                "distanceToSupportPct":  2.2637
+                            },
+                   "2303":  {
+                                "latestPrice":  75.3,
+                                "targetPrice":  79.7,
+                                "supportPrice":  71.125,
+                                "potentialPct":  5.8433,
+                                "rr":  1.0539,
+                                "distanceToSupportPct":  5.5445
+                            },
+                   "2474":  {
+                                "latestPrice":  210.5,
+                                "targetPrice":  216.0,
+                                "supportPrice":  189.5,
+                                "potentialPct":  2.6128,
+                                "rr":  0.2619,
+                                "distanceToSupportPct":  9.9762
+                            },
+                   "3045":  {
+                                "latestPrice":  112.5,
+                                "targetPrice":  119.0,
+                                "supportPrice":  108.5,
+                                "potentialPct":  5.7778,
+                                "rr":  1.625,
+                                "distanceToSupportPct":  3.5556
+                            },
+                   "8383":  {
+                                "latestPrice":  71.1,
+                                "targetPrice":  66.2595,
+                                "supportPrice":  58.8321,
+                                "potentialPct":  -6.808,
+                                "rr":  -0.3946,
+                                "distanceToSupportPct":  17.2544
+                            },
+                   "2401":  {
+                                "latestPrice":  25.2,
+                                "targetPrice":  25.5,
+                                "supportPrice":  22.8018,
+                                "potentialPct":  1.1905,
+                                "rr":  0.1251,
+                                "distanceToSupportPct":  9.5167
+                            },
+                   "3317":  {
+                                "latestPrice":  53.6,
+                                "targetPrice":  58.7,
+                                "supportPrice":  50.5,
+                                "potentialPct":  9.5149,
+                                "rr":  1.6452,
+                                "distanceToSupportPct":  5.7836
+                            },
+                   "2392":  {
+                                "latestPrice":  39.6,
+                                "targetPrice":  42.25,
+                                "supportPrice":  35.85,
+                                "potentialPct":  6.6919,
+                                "rr":  0.7067,
+                                "distanceToSupportPct":  9.4697
+                            },
+                   "2855":  {
+                                "latestPrice":  33.2,
+                                "targetPrice":  34.15,
+                                "supportPrice":  30.85,
+                                "potentialPct":  2.8614,
+                                "rr":  0.4043,
+                                "distanceToSupportPct":  7.0783
+                            },
+                   "6152":  {
+                                "latestPrice":  16.25,
+                                "targetPrice":  18.7,
+                                "supportPrice":  15.5,
+                                "potentialPct":  15.0769,
+                                "rr":  3.2667,
+                                "distanceToSupportPct":  4.6154
+                            }
                }
 };
 
